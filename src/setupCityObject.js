6b94ecb8ca33c7e9875b04ad841044f36e91a406
vitmonjo <vitmonjo@gmail.com>
2023-08-17 08:10:51 -0300

    Add all relevant information to cityObject

--- src/setupCityObject.js
+++ src/setupCityObject.js
@@ -1,18 +1,19 @@
 export async function setupCityObject(cityData) {
 
-    // let cityObject = {};
-
-    // setTimeout(() => {
-    //     cityObject.condition = cityData.current.condition.text,
-    //     cityObject.icon = cityData.current.condition.icon,
-    //     cityObject.temp = {
-    //             celsius: cityData.current.temp_c,
-    //             fahrenheit: cityData.current.temp_f
-    //         }
-    //     }, 1000)
-
-
-    // return cityObject;
+    return new Promise((resolve, reject) => {
+        const cityObject = {};
+        cityObject.condition = cityData.current.condition.text;
+        cityObject.icon = cityData.current.condition.icon;
+        cityObject.countryName = cityData.location.country;
+        cityObject.cityName = cityData.location.name;
+        cityObject.region = cityData.location.region;
+        cityObject.localtime = cityData.location.localtime;
+        cityObject.temp = {
+                celsius: cityData.current.temp_c,
+                fahrenheit: cityData.current.temp_f
+            };
+        resolve(cityObject);
+    })
 
 }
 
